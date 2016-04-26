d784db0cf2db85046b68cbd7d31f76e1e0882325
vinay mavi <vinaymavi@gmail.com>
2016-04-26 09:23:10 +0530

    Page render from templage.

--- README.md
+++ README.md
@@ -2,11 +2,16 @@
 egaav server app engine code.
 
 commands 
--------------------------------------
+--------
+* activate virtual environment source egaav/bin/activate
 * local server dev_appserver.py <app.yaml dir path>
 * deployment gcloud preview app deploy app.yaml --promote
 
 Urls
-------------------
+----
 * https://egaav-prod.appspot.com/
 
+Installation
+------------
+* webapp2 **pip install webapp2**
+* google python client **pip install --upgrade google-api-python-client**
--- index.py
+++ index.py
@@ -1,9 +1,22 @@
+import os
 import webapp2
+import jinja2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+        extensions=['jinja2.ext.autoescape'],
+        autoescape=True)
+
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        template_values = {
+            'msg': "e-gaav application under development",
+            'title': 'e-gaav under development'
+        }
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
